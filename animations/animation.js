1742e078b77892ce0f8a1d884b1b0b05e17d8c07
Keeven Oliveira <66172398+KeevenOliveira@users.noreply.github.com>
2021-07-25 21:53:37 -0300

    squares in less width

--- animations/animation.js
+++ animations/animation.js
@@ -1,28 +1,58 @@
 
 const ulSquares = document.querySelector("ul.squares")
+const ScreenWindow = screen.width
 
-for (let i = 0; i < 50; i++){
+console.log(ScreenWindow)
 
-    const li = document.createElement("li");
-
-    const random = (min, max)=> Math.random() * (max - min) + min;
-
-    const size = Math.floor(random(10, 90))
-    const position = random(1, 99);
-
-    const delay = random(5, 0.1)
-    const duration = random(24, 12)
-
-    li.style.width = `${size}px`
-    li.style.height = `${size}px`
-    li.style.bottom = `-${size}px`
-
-    li.style.left = `${position}%` 
-
-    li.style.animationDelay = `${delay}s`;
-    li.style.animationDuration = `${duration}s`;
-    li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
-
-
-    ulSquares.appendChild(li);
+if(ScreenWindow < 800){
+    for (let i = 0; i < 25; i++){
+    
+        const li = document.createElement("li");
+    
+        const random = (min, max)=> Math.random() * (max - min) + min;
+    
+        const size = Math.floor(random(10, 90))
+        const position = random(1, 99);
+    
+        const delay = random(5, 0.4)
+        const duration = random(24, 12)
+    
+        li.style.width = `${size}px`
+        li.style.height = `${size}px`
+        li.style.bottom = `-${size}px`
+    
+        li.style.left = `${position}%` 
+    
+        li.style.animationDelay = `${delay}s`;
+        li.style.animationDuration = `${duration}s`;
+        li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
+    
+        ulSquares.appendChild(li);
+    }
+}else{
+    for (let i = 0; i < 50; i++){
+    
+        const li = document.createElement("li");
+    
+        const random = (min, max)=> Math.random() * (max - min) + min;
+    
+        const size = Math.floor(random(10, 90))
+        const position = random(1, 99);
+    
+        const delay = random(5, 0.1)
+        const duration = random(24, 12)
+    
+        li.style.width = `${size}px`
+        li.style.height = `${size}px`
+        li.style.bottom = `-${size}px`
+    
+        li.style.left = `${position}%` 
+    
+        li.style.animationDelay = `${delay}s`;
+        li.style.animationDuration = `${duration}s`;
+        li.style.animationTimingFunction = `cubic-bezier(${Math.random()}, ${Math.random()}, ${Math.random()}, ${Math.random()})`
+    
+    
+        ulSquares.appendChild(li);
+    }
 }
